e7dccb31fb6351ba3d4044c157b067d520893076
Alexey Boklogov <kashira87@mail.ru>
2021-08-01 18:08:15 +0300

    развернул react

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  // render() {
+  //   return (  );
+  // }
+}
+
+export default App;
