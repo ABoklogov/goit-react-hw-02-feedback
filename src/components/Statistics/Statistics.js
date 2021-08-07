53458f601886b393024ab1b4869b4ecc3863b179
Alexey Boklogov <kashira87@mail.ru>
2021-08-07 23:38:14 +0300

    добавляет компоненты StatisticsItem

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,23 +1,36 @@
 import PropTypes from 'prop-types';
 import './Statistics.scss';
-import StatisticsList from './StatisticsList';
+import StatisticsItem from './StatisticsItem';
 import Notification from './Notification';
 
-const Statistics = ({ total, ...otherProps }) => {
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <div>
       <h2>Statistics</h2>
       {total === 0 ? (
         <Notification message="No feedback given" />
       ) : (
-        <StatisticsList total={total} {...otherProps} />
+        <ul className="statistics-list">
+          <StatisticsItem lebel="Good" value={good} />
+          <StatisticsItem lebel="Neutral" value={neutral} />
+          <StatisticsItem lebel="Bad" value={bad} />
+          <StatisticsItem lebel="Total" value={total} />
+          <StatisticsItem
+            lebel="Positive feedback"
+            value={total === 0 ? 0 : `${positivePercentage}%`}
+          />
+        </ul>
       )}
     </div>
   );
 };
 
 Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
   total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
 
 export default Statistics;
